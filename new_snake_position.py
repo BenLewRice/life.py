5029b3850665688368cd8c224a7110462eefd8a9
David Michael Sullivan <dms5@crystal.clear.rice.edu>
2013-03-25 12:58:08 -0500

    Ben Lewis added new_snake_position

---
+++ new_snake_position.py
@@ -0,0 +1,29 @@
+
+### V1: Returns the position number of the energy closest to 9 ###
+
+def new_snake_position(neighbors_matrix):
+    position_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    new_position = 5                            #default movement
+    for i in range(0,3):                        #parse through rows
+        for j in range(0,3):                    #parse through columns
+            if neighbors_matrix[i][j] == 'E':   #check for energy
+                new_position = int(position_matrix[i][j])
+    return(new_position)
+
+
+
+### V2: Creates a list of all energy positions and returns one randomly ###
+
+from random import choice
+
+def new_snake_position(neighbors_matrix):
+    position_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    new_position = 5                            #default movement
+    energy_positions = []
+    for i in range(0,3):                        #parse through rows
+        for j in range(0,3):                    #parse through columns
+            if neighbors_matrix[i][j] == 'E':   #check for energy
+                energy_positions.append(int(position_matrix[i][j]))
+    if len(energy_positions)>=1:
+        new_position = choice(energy_positions)
+    return(new_position)
